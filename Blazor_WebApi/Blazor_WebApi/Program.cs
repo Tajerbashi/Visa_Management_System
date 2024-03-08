f57b11510b9aa12629da451e49493832b890767e
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-03-08 17:52:52 +0330

    Feature : Sync in Local and Update Packages

--- Blazor_WebApi/Blazor_WebApi/Program.cs
+++ Blazor_WebApi/Blazor_WebApi/Program.cs
@@ -1,9 +1,7 @@
 using Blazor_Infrastructure_Library.DatabaseContext;
-using Blazor_WebApi.Client.Pages;
 using Blazor_WebApi.Components;
 using Blazor_WebApi.Middleware;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,41 +9,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DbContextApplication>(options =>
 {
     //options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection"));
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-});
-
 
 var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
-app.UseMiddleware<ExceptionHandler>();  // Type 1
-app.UseRestoreDataHandler();    // Type 2
+//app.UseMiddleware<ExceptionHandler>();  // Type 1
+//app.UseRestoreDataHandler();    // Type 2
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
