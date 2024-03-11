979e48cf33661084593ddc2f1dd5bcc4dca04ca2
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-03-11 22:37:16 +0330

    Feature : Add Service Model

---
+++ Client_Service_GEN/Models/ResultModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Client_Service_GEN.Models
+{
+    public class ResultModel<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public bool Status { get; set; }
+    }
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public string Family{ get; set; }
+        [EmailAddress]
+        public string Email{ get; set; }
+        [PasswordPropertyText]
+        public string Password{ get; set; }
+    }
+}
---
+++ Client_Service_GEN/Repositories/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using Client_Service_GEN.Models;
+
+namespace Client_Service_GEN.Repositories
+{
+    public interface IClientRepository
+    {
+        ResultModel<List<UserModel>> GetAllUsers();
+
+    }
+}
