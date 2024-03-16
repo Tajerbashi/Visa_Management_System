94088daeee272004de6d8ebbaeaf8f5918ac2c70
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-03-17 01:28:10 +0330

    Fix : Check Identity Entities

--- Blazor_Domain_Library/Entities/Security/UserEntity.cs
+++ Blazor_Domain_Library/Entities/Security/UserEntity.cs
@@ -7,6 +7,13 @@ namespace Blazor_Domain_Library.Entities.Security
     [Table("User", Schema = "Security"), Description("کاربران")]
     public class UserEntity : IdentityUser<long>
     {
+        [Description("نام")]
+        public string Name { get; set; }
+
+        [Description("فامیل")]
+        public string Family { get; set; }
+
+
         [Description("حذف شده"), DefaultValue(false)]
         public bool IsDeleted { get; set; }
 
