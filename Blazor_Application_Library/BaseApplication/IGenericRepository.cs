7e1666d1cff239ca38fa3d030b8fa0297c9d21ab
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-03-21 07:49:22 +0330

    Feature : Create Generic Repository

---
+++ Blazor_Application_Library/BaseApplication/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor_Application_Library.BaseApplication
+{
+    public interface IGenericRepository<TEntity, TDTO, TView>
+    {
+        void Create(TEntity entity);
+        List<TEntity> Read();
+        TEntity Read(object ID);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(object ID);
+        bool Exist(long id);
+    }
+}
