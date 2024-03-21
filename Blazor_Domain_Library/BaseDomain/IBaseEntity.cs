1e04cada0f6419ff14657a70fa2366d353b7318f
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-03-21 07:49:47 +0330

    Fix : Some Changes

---
+++ Blazor_Domain_Library/BaseDomain/IBaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blazor_Domain_Library.BaseDomain
+{
+    public interface IBaseEntity
+    {
+
+    }
+    public abstract class BaseEntity<TKey> : IBaseEntity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
+        public TKey ID { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateData { get; set; }
+    }
+    public abstract class BaseEntity : BaseEntity<long>
+    {
+
+    }
+}
