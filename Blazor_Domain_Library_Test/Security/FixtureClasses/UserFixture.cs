f157c59282370f6f933f58d5b334e314fb60eb0f
KTajerbashi <kamrantajerbashi@gmail.com>
2024-03-24 00:12:52 +0330

    Feature : Create Test For Domain

---
+++ Blazor_Domain_Library_Test/Security/FixtureClasses/UserFixture.cs
@@ -0,0 +1,9 @@
+﻿using Blazor_Domain_Library_Test.Security.Services;
+
+namespace Blazor_Domain_Library_Test.Security.FixtureClasses
+{
+    public class UserFixture
+    {
+        public CalculatorService calculatorService = new CalculatorService();
+    }
+}
---
+++ Blazor_Domain_Library_Test/Security/Repositories/ICalculatorRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Blazor_Domain_Library_Test.Security.Repositories
+{
+    public interface ICalculatorRepository
+    {
+    }
+}
