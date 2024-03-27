6f86b969ae3876c4d098e3f1d67d530f019d4b09
Tajerbashi <kamrantajerbashi@gmail.com>
2024-03-27 12:32:34 +0330

    Feature : Add Main SSO Project

---
+++ SSO_Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSO_Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
