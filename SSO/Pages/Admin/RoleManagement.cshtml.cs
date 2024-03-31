57cac8e725a950354a2ce3fedff0b61099026bcc
KTajerbashi <kamrantajerbashi@gmail.com>
2024-03-31 05:15:11 +0330

    Feature : Create Admin Manage On Users and Roles

---
+++ SSO/Pages/Admin/RoleManagement.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SSO.Pages.Admin
+{
+    public class RoleManagementModel : PageModel
+    {
+        public long Id { get; set; }
+        public string Mode { get; set; }
+
+        public void OnGet(long id)
+        {
+            Id = id;
+            Mode = "صفحه اولیه";
+        }
+        public void OnGetCreate(long id)
+        {
+            Id = id;
+            Mode = "ایجاد کردن";
+        }
+        public void OnGetUpdate(long id)
+        {
+            Id = id;
+            Mode = "ویرایش کردن";
+        }
+        public void OnGetDelete(long id)
+        {
+            Id = id;
+            Mode = "حذف کردن";
+        }
+        public void OnGetRead(long id)
+        {
+            Id = id;
+            Mode = "خواندن";
+        }
+        public void OnGetAddRoleToUser(long id)
+        {
+            Id = id;
+            Mode = "تعریف کاربر برای این نقش";
+        }
+        public void OnGetUsersOfRole(long id)
+        {
+            Id = id;
+            Mode = "کاربران این نقش";
+        }
+        public void OnGetPrivilege(long id)
+        {
+            Id = id;
+            Mode = "مدیریت دسترسی های این نقش";
+        }
+    }
+}
---
+++ SSO/Pages/Admin/UserManagement.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SSO.Pages.Admin
+{
+    public class UserManagementModel : PageModel
+    {
+        public long Id { get; set; }
+        public string Mode { get; set; }
+        //public void OnGet(string id)
+        //{
+        //    Id = id;
+        //}
+
+        public void OnGet(long id)
+        {
+            Id = id;
+            Mode = "صفحه اولیه";
+        }
+
+        public void OnGetCreate(long id)
+        {
+            Id = id;
+            Mode = "ایجاد کردن";
+        }
+
+        public void OnGetUpdate(long id)
+        {
+            Id = id;
+            Mode = "ویرایش کردن";
+        }
+
+        public void OnGetDelete(long id)
+        {
+            Id = id;
+            Mode = "حذف کردن";
+        }
+
+        public void OnGetRead(long id)
+        {
+            Id = id;
+            Mode = "خواندن";
+        }
+    }
+}
