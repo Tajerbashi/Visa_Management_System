26b0f0bd33fa7228873e8724ed526b1f0700dd0c
KTajerbashi <kamrantajerbashi@gmail.com>
2024-04-01 02:55:02 +0330

    Feature : Create Profile Page

---
+++ SSO/Pages/Identity/ProfilePage.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SSO.Pages.Identity
+{
+    public class ProfilePageModel : PageModel
+    {
+
+        public void OnGet(string name)
+        {
+        }
+    }
+}
