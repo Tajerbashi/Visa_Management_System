7332b5d25b0380487b0076789b1e59068c4d948f
KTajerbashi <kamrantajerbashi@gmail.com>
2024-04-08 03:23:30 +0330

    Feature : Blazor Server App Configuration

--- Blazor_WebApi/Program.cs
+++ Blazor_WebApi/Program.cs
@@ -1,17 +1,20 @@
-using Blazor_WebApi.Components;
+using Blazor_WebApi.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.Configure<RazorPagesOptions>(options =>
+options.RootDirectory = "/Components");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -19,9 +22,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/Views/_Host");
 
 app.Run();
