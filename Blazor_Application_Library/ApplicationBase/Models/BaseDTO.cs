32fab8add622e77bba04f31787e93f3fa401b9aa
KTajerbashi <kamrantajerbashi@gmail.com>
2024-04-09 21:53:50 +0330

    Feature : Add Some DTOs and Base Model

---
+++ Blazor_Application_Library/ApplicationBase/Models/BaseDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_Application_Library.ApplicationBase.Models
+{
+    public interface IBaseDTO<TKey>
+    {
+        TKey ID { get; set; }
+    }
+    public class BaseDTO<T> : IBaseDTO<T>
+    {
+        public T ID { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+    public class BaseDTO : BaseDTO<long>
+    {
+
+    }
+}
---
+++ Blazor_Application_Library/Models/Security/SignUpDTO.cs
@@ -0,0 +1,15 @@
+﻿using Blazor_Application_Library.ApplicationBase.Models;
+
+namespace Blazor_Application_Library.Models.Security
+{
+    public class SignUpDTO : BaseDTO
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+    }
+}
