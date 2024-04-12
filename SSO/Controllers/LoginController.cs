041d24d2b9e4d5ccabdaa1b8979d6d007270b97b
KTajerbashi <kamrantajerbashi@gmail.com>
2024-04-13 00:47:44 +0330

    Feature : Use CancellationToken in SSO Project

--- SSO/Controllers/LoginController.cs
+++ SSO/Controllers/LoginController.cs
@@ -18,7 +18,26 @@ namespace SSO.Controllers
         {
             return View();
         }
-        
+        [HttpGet("Report")]
+        public async Task<string> Report(CancellationToken cancellationToken)
+        {
+            var startTime = DateTime.Now;
+            _logger.LogInformation($"Starting Request : {DateTime.Now}");
+            for (int i = 0; i < 5; i++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                _logger.LogInformation(i.ToString());
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation($"cancellationToken.IsCancellationRequested : {cancellationToken.IsCancellationRequested}");
+                }
+                await Task.Delay(1000, cancellationToken);
+            }
+            _logger.LogInformation($"Finished Request : {DateTime.Now}");
+            var endTime = DateTime.Now;
+            var message = $"Start Time :  {startTime} \nEnd Time :  {endTime}";
+            return message;
+        }
         [Route("Login")]
         [HttpPost]
         public IActionResult Login(LoginDTO model)
